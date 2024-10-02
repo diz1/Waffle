dc644ca899a0cec51b62588223a1fe3ece93feae
Dmitriy Smirnov <dima-smirnov1@mail.ru>
2024-10-03 01:58:28 +0300

    fix icon and mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -140,9 +140,6 @@ document.addEventListener('DOMContentLoaded', () => {
           dots: true,
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   })
 })
